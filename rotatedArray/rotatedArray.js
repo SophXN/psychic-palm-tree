d68b1e388d45177ca661f880479be3186dec1ea0
Sophie Nguyen <sophie@outerlabs.io>
2022-05-22 16:08:59 -0400

    rotatedArray

---
+++ rotatedArray/rotatedArray.js
@@ -0,0 +1,15 @@
+// Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+const rotate = function (nums, k) {
+  for (i = 0; i < k; i++) {
+    const lastItem = nums.pop();
+    nums.unshift(lastItem);
+    rotateCount--;
+  }
+  // it says to return nothing
+  return;
+};
+
+console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));
+console.log(rotate([4, 1, 7, 13, 7], 23));
+console.log(rotate([-1, -100, 3, 99], 2));
