d7a455b4c8378100244454795ba11f1089d4f45e
Sophie Nguyen <sophie@outerlabs.io>
2022-05-24 18:12:06 -0400

    speed up rotateArray

--- rotatedArray/rotatedArray.js
+++ rotatedArray/rotatedArray.js
@@ -1,16 +1,14 @@
 // Given an array, rotate the array to the right by k steps, where k is non-negative.
 
 const rotate = function (nums, k) {
-    let 
-  for (i = 0; i < k; i++) {
-    const lastItem = nums.pop();
-    nums.unshift(lastItem);
-    rotateCount--;
+  k %= nums.length;
+  if (k !== 0) {
+    var tmp = nums.slice(-k);
+    nums.splice(-k, k);
+    Array.prototype.unshift.apply(nums, tmp);
   }
-  // it says to return nothing
-  return;
 };
 
-console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));
-console.log(rotate([4, 1, 7, 13, 7], 23));
-console.log(rotate([-1, -100, 3, 99], 2));
+rotate([1, 2, 3, 4, 5, 6, 7], 3);
+rotate([1, 2, 3], 5);
+rotate([-1, -100, 3, 99], 2);
