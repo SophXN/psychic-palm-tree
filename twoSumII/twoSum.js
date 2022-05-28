93f996d295635d3b73de501dbdc03868b8534e41
Sophie Nguyen <nguyen.x.sophia@gmail.com>
2022-05-28 09:25:31 -0400

    updating github account

--- twoSumII/twoSum.js
+++ twoSumII/twoSum.js
@@ -18,27 +18,3 @@ const twoSum = (numbers, target) => {
 console.log(twoSum([7, 0, 0, 0, 500, 2, 11, 15], 9));
 console.log(twoSum([0, 0, 3, 4], 0));
 console.log(twoSum([0, 3, 3, 4], 6));
-// console.log(twoSum([-1, 0], -1));
-
-/**
- *  let resultArr = [];
-
-  for (i = 0; i < numbers.length; i++) {
-    const diff = target - numbers[i];
-    let meow = numbers.indexOf(diff);
-    if (meow !== -1) {
-      let secondIndex = meow;
-
-      // find second occurrance
-      if (secondIndex === i) {
-        let slice = numbers.slice(secondIndex + 1);
-        let secondOccurrance = slice.indexOf(diff);
-        secondIndex = secondOccurrance + secondIndex + 1;
-      }
-      resultArr = [i + 1, secondIndex + 1];
-      break;
-    }
-  }
-
-  return resultArr;
- */
