6dff5171507e2e9e06832ab849c42b8076b69f76
Sophie Nguyen <nguyen.x.sophia@gmail.com>
2022-05-28 09:48:16 -0400

    reverseString JS

---
+++ reverseString/reverseString.js
@@ -0,0 +1,23 @@
+/*
+Write a function that reverses a string. The input string is given as an array of 
+characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+*/
+
+const reverseString = (s) => {
+  let i = 0;
+  let j = s.length - 1;
+  while (i < j) {
+    const tempStart = s[i];
+    const tempEnd = s[j];
+    s[i] = tempEnd;
+    s[j] = tempStart;
+    i++;
+    j--;
+  }
+};
+
+console.log(reverseString(["h", "e", "l", "l", "o"]));
+
+// start with a pointer at the left and right end and switch the items
